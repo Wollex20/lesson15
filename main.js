730eb3440d9e15d708de1d477110ca171df2b6dc
Wollex20 <luarsabishvili.s20@gmail.com>
2024-11-28 20:34:02 +0400

    Done

---
+++ main.js
@@ -0,0 +1,16 @@
+let userName = "Shota Luarsabishvili";
+let dayofWeek = "Thursday";
+let favoriteActivity = "play videogame";
+// console.log(userName, dayofWeek, favoriteActivity);
+console.log(
+  "Hi this is second text and my is",
+  userName,
+  "today is",
+  dayofWeek,
+  "a perfect day",
+  favoriteActivity
+);
+
+let textUser =
+  "Hi my name is 'Shota Luarsabishvili' Today is 'dayofweek', a perfect day for 'play football'";
+console.log(textUser);
